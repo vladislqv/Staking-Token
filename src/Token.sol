8c12edfd1dfed3ad91f257b411b2abdf0309c47e
Vladislav Zavtur <ychoeth@proton.me>
2024-02-29 00:08:11 +0200

    commit

---
+++ src/Token.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+    constructor(address initialOwner)
+        ERC20("MyToken", "MTK")
+        Ownable(initialOwner)
+    {
+        _mint(initialOwner, 1000000 * (10 ** uint256(decimals())));
+    }
+}
